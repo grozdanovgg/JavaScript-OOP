d4a7ba881a7a38dc081783ccddf1298e56966510
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-01-31 21:16:18 +0200

    Solve task from homework 02

--- Topics/02. Closures-and-Scope/homework/tasks/task-1.js
+++ Topics/02. Closures-and-Scope/homework/tasks/task-1.js
@@ -1,41 +1,53 @@
-/* Task Description */
-/* 
-	*	Create a module for working with books
-		*	The module must provide the following functionalities:
-			*	Add a new book to category
-				*	Each book has unique title, author and ISBN
-				*	It must return the newly created book with assigned ID
-				*	If the category is missing, it must be automatically created
-			*	List all books
-				*	Books are sorted by ID
-				*	This can be done by author, by category or all
-			*	List all categories
-				*	Categories are sorted by ID
-		*	Each book/catagory has a unique identifier (ID) that is a number greater than or equal to 1
-			*	When adding a book/category, the ID is generated automatically
-		*	Add validation everywhere, where possible
-			*	Book title and category name must be between 2 and 100 characters, including letters, digits and special characters ('!', ',', '.', etc)
-			*	Author is any non-empty string
-			*	Unique params are Book title and Book ISBN
-			*	Book ISBN is an unique code that contains either 10 or 13 digits
-			*	If something is not valid - throw Error
-*/
 function solve() {
 	var library = (function () {
 		var books = [];
 		var categories = [];
-		function listBooks() {
-			return books;
-		}
+		const minTitleLength = 2,
+			maxTitleLength = 100;
 
 		function addBook(book) {
+			if (book.title.length < minTitleLength || book.title.length > maxTitleLength) {
+				throw new Error("Title must have between 2 and 100 characters!")
+			}
+			if (book.author === '') {
+				throw new Error("Author name must not be empty!")
+			}
+			let isIsbnNotUnique = books.find(x => x.isbn === book.isbn),
+				isIsbnLength10 = (book.isbn.length === 10),
+				isIsbnLength13 = (book.isbn.length === 13);
+			isBookTitleNotUnique = books.find(x => x.title === book.title)
+			if (isIsbnNotUnique) {
+				throw new Error('ISBN number is not unique')
+			}
+			if (!isIsbnLength10 && !isIsbnLength13) {
+				throw new Error(`ISBN length is ${book.isbn.length}. The length must be exactly 10 or 13 characters`)
+			}
+			if (isBookTitleNotUnique) {
+				throw new Error(`Book title {book.title} is not unique`)
+			}
+
+			if (!categories.some(x => x === book.category)) {
+				categories.push(book.category)
+			}
 			book.ID = books.length + 1;
 			books.push(book);
 			return book;
 		}
 
+		function listBooks() {
+			if (arguments[0]) {
+				if (arguments[0].hasOwnProperty("category")) {
+					return books.filter(x => x.category === arguments[0].category)
+				}
+				if (arguments[0].hasOwnProperty("author")) {
+					return books.filter(x => x.author === arguments[0].author)
+				}
+			}
+			return books;
+		}
+
 		function listCategories() {
-			return categories;
+			return categories.sort();
 		}
 
 		return {
@@ -47,7 +59,19 @@ function solve() {
 				list: listCategories
 			}
 		};
-	} ());
+	}());
 	return library;
 }
+
+// var book1 = {
+// 		title: 'the good parts',
+// 		isbn: '1234567890',
+// 		author: 'Gosho',
+// 		category: 'triler'
+// 	},
+// 	testLibrary = solve();
+
+// testLibrary.books.add(book1);
+// console.log(testLibrary.books.list());
+// console.log(testLibrary.categories.list());
 module.exports = solve;
