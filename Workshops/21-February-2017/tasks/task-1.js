544c8f73ae2c6dcbffe1db4fa349d0af9ef96e67
grozdanovgg <grozdanovgg@gmail.com>
2017-02-21 18:58:21 +0200

    work in progress

--- Workshops/21-February-2017/tasks/task-1.js
+++ Workshops/21-February-2017/tasks/task-1.js
@@ -1,20 +1,51 @@
 function solve() {
-	const module = {
-		getPlayer: function (name){
-			// returns a new player instance with the provided name
-		},
-		getPlaylist: function(name){
-			//returns a new playlist instance with the provided name
-		},
-		getAudio: function(title, author, length){
-			//returns a new audio instance with the provided title, author and length
-		},
-		getVideo: function(title, author, imdbRating){
-			//returns a new video instance with the provided title, author and imdbRating
-		}
-	};
+    let getID = (function() {
+        let id = 0;
 
-	return module;
+        return function() {
+            id += 1;
+            return id;
+        };
+    }());
+    const VALIDATOR = {
+
+    }
+    class Player {
+        set name(name) {
+            this._name = name;
+        }
+        get name(name) {
+            return this._name;
+        }
+
+        addPlaylist(playlistToAdd) {
+
+        }
+    }
+    class PlayList {
+        constructor() {
+            this._id = getid();
+        }
+
+    }
+
+    const module = {
+
+        getPlayer: function(name) {
+            // returns a new player instance with the provided name
+        },
+        getPlaylist: function(name) {
+            //returns a new playlist instance with the provided name
+        },
+        getAudio: function(title, author, length) {
+            //returns a new audio instance with the provided title, author and length
+        },
+        getVideo: function(title, author, imdbRating) {
+            //returns a new video instance with the provided title, author and imdbRating
+        }
+    };
+
+    return module;
 }
 
 module.exports = solve;
